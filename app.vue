ed77652bf572037cc513aee3edc71619064a5150
Fariz <nizaralfariziakbar10@gmail.com>
2023-07-14 17:01:56 +0700

    test manual manifest

--- app.vue
+++ app.vue
@@ -1,4 +1,4 @@
-<script setup lang="ts">
+<script setup>
 onMounted(() => {
   if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
     document.documentElement.classList.add('dark')
@@ -12,7 +12,7 @@ onMounted(() => {
 </script>
 
 <template>
-  <VitePwaManifest/>
+  <!-- <VitePwaManifest/> -->
   <div class="bg-gray-100 min-h-screen">
     <NuxtLayout>
       <NuxtPage />
@@ -20,15 +20,3 @@ onMounted(() => {
     </NuxtLayout>
   </div>
 </template>
-
-<style>
-.page-enter-active,
-.page-leave-active {
-  transition: all 0.4s;
-}
-.page-enter-from,
-.page-leave-to {
-  opacity: 0;
-  filter: blur(1rem);
-}
-</style>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,8 +5,13 @@ export default defineNuxtConfig({
       title: "Daily",
       charset: "utf-8",
       viewport: "width=device-width, initial-scale=1",
+      link: [
+        {
+          rel: 'manifest',
+          href: '/manifest.json'
+        },
+      ],
     },
-    pageTransition: { name: "page", mode: "out-in" },
   },
   css: ["~/assets/css/fonts.css", "~/assets/css/main.css"],
   devtools: { enabled: true },
@@ -19,46 +24,43 @@ export default defineNuxtConfig({
   build: {
     transpile: ["vue-sonner"],
   },
-  modules: ["@vite-pwa/nuxt"],
-  pwa: {
-    manifest: {
-      name: "Daily",
-      short_name: "Daily",
-      description: "Fariz daily log.",
-      icons: [
-        {
-          src: 'pwa-64x64.png',
-          sizes: '64x64',
-          type: 'image/png'
-        },
-        {
-          src: 'pwa-192x192.png',
-          sizes: '192x192',
-          type: 'image/png'
-        },
-        {
-          src: 'pwa-512x512.png',
-          sizes: '512x512',
-          type: 'image/png',
-          purpose: 'any'  
-        },
-        {
-          src: 'maskable-icon-512x512.png',
-          sizes: '512x512',
-          type: 'image/png',
-          purpose: 'maskable'
-        }
-      ]
-    },
-    workbox: {
-      navigateFallback: "/",
-      // globDirectory: "/Users/fariz/projects/mine/daily/.nuxt/dev-sw-dist",
-      // globPattern: "**/*.{js,css,html}",
-      // globIgnores: ["**/node_modules/**/*", "sw.js", "workbox-*.js"],
-    },
-    devOptions: {
-      enabled: true,
-      type: "module",
-    },
-  },
+  // modules: ["@vite-pwa/nuxt"],
+  // pwa: {
+  //   manifest: {
+  //     name: "Daily",
+  //     short_name: "Daily",
+  //     description: "Fariz daily log.",
+  //     icons: [
+  //       {
+  //         src: 'pwa-64x64.png',
+  //         sizes: '64x64',
+  //         type: 'image/png'
+  //       },
+  //       {
+  //         src: 'pwa-192x192.png',
+  //         sizes: '192x192',
+  //         type: 'image/png'
+  //       },
+  //       {
+  //         src: 'pwa-512x512.png',
+  //         sizes: '512x512',
+  //         type: 'image/png',
+  //         purpose: 'any'  
+  //       },
+  //       {
+  //         src: 'maskable-icon-512x512.png',
+  //         sizes: '512x512',
+  //         type: 'image/png',
+  //         purpose: 'maskable'
+  //       }
+  //     ]
+  //   },
+  //   workbox: {
+  //     navigateFallback: "/",
+  //   },
+  //   devOptions: {
+  //     enabled: true,
+  //     type: "module",
+  //   },
+  // },
 });
